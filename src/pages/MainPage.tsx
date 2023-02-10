54e19f8a80eb153f6442078562c1fce3809c8857
gacmen45 <marekgacek45@gmail.com>
2023-02-10 07:04:30 +0100

    create template of mainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {Grid, Typography} from '@mui/material/';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
 
 const  MainPage=()=> {
   return (
-    <div>Main</div>
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+        <Card>
+          <CardHeader title='1'/>
+          <CardMedia component='img' height='190' image='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg'/>
+          <CardContent>
+            <Typography variant='h6'>Bulbasaur</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+     
+    </Grid>
   )
 }
 
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { AppBar, Toolbar,Container } from '@mui/material'
 
-const RootLayout =() =>{
+const RootLayout = () => {
 	return (
 		<>
-        <h1>Test</h1>
-			<main>
+			<AppBar position='sticky' sx={{ backgroundColor: '#ef5350' }}>
+				<Toolbar></Toolbar>
+			</AppBar>
+			<h1>Test</h1>
+			<Container>
 				<Outlet />
-			</main>
+				</Container>
 		</>
 	)
 }
