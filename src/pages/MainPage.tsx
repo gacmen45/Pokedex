af155806154defbe9018b65888b90ebe0fa08243
gacmen45 <marekgacek45@gmail.com>
2023-02-16 08:45:29 +0100

    fix search logic

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -10,12 +10,15 @@ import Pagination from '../components/Pagination'
 const MainPage = () => {
 	const [pokemons, setPokemons] = useState([])
 	const [loading, setLoading] = useState(false)
+	const [inputVal, setInputVal] = useState('')
 
 	const searchPokemons = async pokemon => {
 		try {
-			let url = `https://pokeapi.co/api/v2/pokemon.${pokemon}`
+			let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
 			const response = await fetch(url)
-			return await response.json()
+			const data = await response.json()
+
+			setPokemons([data])
 		} catch (error) {
 			console.log('error: ', error)
 		}
@@ -30,12 +33,12 @@ const MainPage = () => {
 			console.log('error: ', error)
 		}
 	}
-	const getPokemonData = async (url) => {
+	const getPokemonData = async url => {
 		try {
 			const response = await fetch(url)
 			return await response.json()
 		} catch (error) {
-			console.log("error: ", error)
+			console.log('error: ', error)
 		}
 	}
 
@@ -43,26 +46,40 @@ const MainPage = () => {
 		try {
 			setLoading(true)
 			const data = await getPokemons()
-			const promises = data.results.map(async (pokemon) => {
-				return await getPokemonData(pokemon.url);
+			const promises = data.results.map(async pokemon => {
+				return await getPokemonData(pokemon.url)
 			})
 
-			const results = await Promise.all(promises);
-			setPokemons(results);
+			const results = await Promise.all(promises)
+			setPokemons(results)
 			setLoading(false)
 		} catch (error) {
 			console.log(error)
 		}
 	}
 
-	useEffect(() => {
-		fetchPokemons()
-	}, [])
+	const input = e => {
+		setInputVal(e.target.value)
+	}
 
-	console.log(pokemons)
+	const test = pokemon => {
+		if (pokemon === '') {
+			fetchPokemons()
+			console.log(pokemons)
+		} else {
+			searchPokemons(pokemon)
+		}
+	}
+
+	useEffect(() => {
+		// fetchPokemons()
+		test(inputVal)
+	}, [inputVal])
 
 	return (
 		<Container>
+			<input onChange={input} type='text' />
+
 			{/* <Pagination prevPageUrl={prevPageUrl} nextPageUrl={nextPageUrl} totalPages={totalPages} nextPage={nextPage} prevPage={prevPage} page={page}/> */}
 			{/* <button onClick={prevPageUrl ? prevPage : null}>prev</button>
 			<button onClick={nextPageUrl ? nextPage : null}>next</button>
@@ -70,7 +87,7 @@ const MainPage = () => {
 
 			<Grid container spacing='4'>
 				{pokemons.map(pokemon => (
-					<PokemonCard key={pokemon.id} pokemon={pokemon}/>
+					<PokemonCard key={pokemon.id} pokemon={pokemon} />
 				))}
 			</Grid>
 		</Container>
