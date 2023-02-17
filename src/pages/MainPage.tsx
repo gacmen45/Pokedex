5c0727b54a853323a73f72e9c851d8adb96091de
gacmen45 <marekgacek45@gmail.com>
2023-02-17 08:23:16 +0100

    fixing problems with pagination

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -7,27 +7,35 @@ import PokemonCard from '../components/PokemonCard'
 
 import Pagination from '../components/Pagination'
 
+
 const MainPage = () => {
 	const [pokemons, setPokemons] = useState([])
 	const [loading, setLoading] = useState(false)
 	const [inputVal, setInputVal] = useState('')
 
-	const searchPokemons = async pokemon => {
-		try {
-			let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-			const response = await fetch(url)
-			const data = await response.json()
+	const [numberOfPokemon , setNumberOfPokemon] = useState()
+	
+	
+	
+	const [currentPage,setCurrentPage] = useState('https://pokeapi.co/api/v2/pokemon?limit=50')
+	const [nextPageUrl, setNextPageUrl] = useState('')
+	const [prevPageUrl, setPrevPageUrl] = useState('')
+	
 
-			setPokemons([data])
-		} catch (error) {
-			console.log('error: ', error)
-		}
+
+	const nextPage = () => {
+		setCurrentPage(nextPageUrl)
+		
+	}
+	const prevPage = () => {
+		setCurrentPage(prevPageUrl)
+		
 	}
 
 	const getPokemons = async (limit = 50, offset = 0) => {
 		try {
 			let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
-			const response = await fetch(url)
+			const response = await fetch(currentPage)
 			return await response.json()
 		} catch (error) {
 			console.log('error: ', error)
@@ -42,10 +50,23 @@ const MainPage = () => {
 		}
 	}
 
+	const searchPokemons = async pokemon => {
+		try {
+			let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+			const response = await fetch(url)
+			const data = await response.json()
+			setPokemons([data])
+		} catch (error) {
+			console.log('error: ', error)
+		}
+	}
+
 	const fetchPokemons = async () => {
 		try {
 			setLoading(true)
 			const data = await getPokemons()
+			setNextPageUrl(data.next)
+			setPrevPageUrl(data.previous)
 			const promises = data.results.map(async pokemon => {
 				return await getPokemonData(pokemon.url)
 			})
@@ -63,22 +84,44 @@ const MainPage = () => {
 	}
 
 	const test = pokemon => {
-		if (pokemon === '') {
+		if (!pokemon) {
+			// if (pokemon === '') {
 			fetchPokemons()
-			console.log(pokemons)
 		} else {
 			searchPokemons(pokemon)
 		}
 	}
 
+	const handler = () => {
+		if (inputVal === '') {
+			fetchPokemons()
+		} else searchPokemons(inputVal)
+	}
+	const showAll = () => {
+		fetchPokemons()
+		setInputVal('')
+		setCurrentPage('https://pokeapi.co/api/v2/pokemon?limit=20')
+	}
+
+
+const handlerNumber = (e) => {
+	setNumberOfPokemon(e.target.value)
+}
+
 	useEffect(() => {
-		// fetchPokemons()
-		test(inputVal)
-	}, [inputVal])
+		fetchPokemons()
+		// test(inputVal)
+		// }, [inputVal])
+	}, [currentPage])
 
 	return (
 		<Container>
-			<input onChange={input} type='text' />
+			<input onChange={input} type='text' value={inputVal} />
+			<button onClick={handler}>search</button>
+			<button onClick={showAll}>showAll</button>
+
+		<button onClick={prevPage}>prev</button>
+		<button onClick={nextPage}>next</button>
 
 			{/* <Pagination prevPageUrl={prevPageUrl} nextPageUrl={nextPageUrl} totalPages={totalPages} nextPage={nextPage} prevPage={prevPage} page={page}/> */}
 			{/* <button onClick={prevPageUrl ? prevPage : null}>prev</button>
