7eacf7b4561eb1e0fe3de265a39827ac9cbc0e70
gacmen45 <marekgacek45@gmail.com>
2023-02-20 08:47:04 +0100

    finish bgc logic

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -4,26 +4,25 @@ import CardMedia from '@mui/material/CardMedia'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
+import styled from '@emotion/styled'
 
 import { useState } from 'react'
 import PokemonModal from './PokemonModal'
 
-// import './PokemonCard.css'
+
+import { CardWrapper } from '../util/CardBgc'
+
 
 const PokemonCard = pokemon => {
 	const { id, name, types, sprites } = pokemon.pokemon
 
-	
-	
-
-
-
 	return (
 		<Grid key={id} item xs={12} sm={6} md={4} lg={3} xl={2}>
-			<Card sx={{ backgroundColor: types[0].type.name === 'grass' ? 'green' : types[0].type.name === 'fire' ? 'red' : types[0].type.name === 'water' ? 'blue' : 'grey' }}>
 			
+			{/* <CardWrapper type={types[0].type.name}> */}
+			<CardWrapper types={types}>
 				<CardHeader title={`#${id}`} />
-				<PokemonModal pokemon={pokemon.pokemon}/>
+				<PokemonModal pokemon={pokemon.pokemon} />
 				<CardMedia component='img' width='150' image={sprites.front_default} />
 				<CardContent>
 					<Typography variant='h5' component='h2'>
@@ -37,7 +36,7 @@ const PokemonCard = pokemon => {
 						))}
 					</ul>
 				</CardContent>
-			</Card>
+			</CardWrapper>
 		</Grid>
 	)
 }
